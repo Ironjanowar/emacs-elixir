acf1d7626c2a3c4e93ec9d68ca7c35a4cddedabb
Ironjanowar <ironjanowar@gmail.com>
2021-04-22 11:56:10 +0200

    Add youtube

---
+++ settings/helm-youtube.el
@@ -0,0 +1,105 @@
+;;; helm-youtube.el --- Query YouTube and play videos in your browser
+
+;; Copyright (C) 2016 Maximilian Roquemore
+
+;; Author: Maximilian Roquemore <maximus12793@gmail.com>
+;; Version: 1.1
+;; Package-Requires: ((request "0.2.0") (helm "2.3.1") (cl-lib "0.5"))
+;; URL: https://github.com/maximus12793/helm-youtube
+;; Created: 2016-Oct-19 01:58:25
+;; Keywords: youtube, multimedia
+
+;;; Commentary:
+
+;; This package provides an interactive prompt to search
+;; youtube as you code :)
+
+;;; MIT License
+;;
+;; Permission is hereby granted, free of charge, to any person obtaining
+;; a copy of this software and associated documentation files (the
+;; "Software"), to deal in the Software without restriction, including
+;; without limitation the rights to use, copy, modify, merge, publish,
+;; distribute, sublicense, and/or sell copies of the Software, and to
+;; permit persons to whom the Software is furnished to do so, subject to
+;; the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+;; LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+;; OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+;; WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+;;; Code:
+
+(use-package cl-lib
+  :ensure t)
+(use-package helm
+  :ensure t)
+(use-package request
+  :ensure t)
+(use-package json
+  :ensure t)
+
+(defgroup helm-youtube nil
+  "Helm-youtube settings."
+  :group 'tools)
+
+;;;###autoload
+(defun helm-youtube()
+  (interactive)
+  (request
+   "https://www.googleapis.com/youtube/v3/search"
+   :params `(("part" . "snippet")
+	     ("q" . ,(read-string "Search YouTube: "))
+	     ("type" . "video")
+	     ("maxResults" . "20")
+	     ("key" .  "AIzaSyDOiRT65ikY28V2sx23nvgSPZ1o8q7qxrM"));; <--- GOOGLE API KEY
+   :parser 'json-read
+   :success (cl-function
+	     (lambda (&key data &allow-other-keys)
+	       (helm-youtube-wrapper data)));;function
+   :status-code '((400 . (lambda (&rest _) (message "Got 400.")))
+		  ;; (200 . (lambda (&rest _) (message "Got 200.")))
+		  (418 . (lambda (&rest _) (message "Got 418."))))
+   :complete (message "searching...")))
+
+(defun helm-youtube-tree-assoc (key tree)
+  "Build the tree-assoc from KEY TREE for youtube query."
+  (when (consp tree)
+    (cl-destructuring-bind (x . y)  tree
+      (if (eql x key) tree
+	(or (helm-youtube-tree-assoc key x) (helm-youtube-tree-assoc key y))))))
+
+(defun helm-youtube-playvideo (video-id)
+  "Format the youtube URL via VIDEO-ID."
+  (shell-command
+   (concat "mpv " "http://www.youtube.com/watch?v=" video-id)))
+
+
+(defun helm-youtube-wrapper (*qqJson*)
+  "Parse the json provided by *QQJSON* and provide search result targets."
+  (let (*results* you-source)
+    (setq *qqJson* (cdr (assoc 'items *qqJson*)))
+    (cl-loop for x being the elements of *qqJson*
+	     do (push (cons (cdr (helm-youtube-tree-assoc 'title x)) (cdr (helm-youtube-tree-assoc 'videoId x))) *results*))
+    (let ((you-source
+	   `((name . "Youtube Search Results")
+	     (candidates . ,(mapcar 'car *results*))
+	     (action . (lambda (candidate)
+			 ;; (message-box "%s" (candidate))
+			 (helm-youtube-playvideo (cdr (assoc candidate *results*)))
+			 )))))
+      (helm :sources '(you-source)))))
+
+(provide 'helm-youtube)
+
+;; Local Variables:
+;; End:
+
+;;; helm-youtube.el ends here
